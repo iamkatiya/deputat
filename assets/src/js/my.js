b1824de976f3abae3ee2aa021593cc14d940b523
iamkatiya <53205411+iamkatiya@users.noreply.github.com>
2020-01-17 15:47:18 +0800

    save

--- assets/src/js/my.js
+++ assets/src/js/my.js
@@ -31,3 +31,27 @@ $('#burger').click(function () {
         $('.burger-line3').addClass('burger-transform-3');
     }
 });
+var show = true;
+$(window).scroll(function () {
+    if (!show) return false;
+    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    var e_top = $('.district-in-numbers-items').offset().top; // Расстояние от блока со счетчиками до верха всего документа
+    var w_height = $(window).height(); // Высота окна браузера
+    var d_height = $(document).height(); // Высота всего документа
+    var e_height = $('.district-in-numbers-items').outerHeight(); // Полная высота блока со счетчиками
+
+    if (w_top + w_height >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+        $('.district-item .number').each(function () {
+            $(this).prop('Counter', 0).animate({
+                Counter: $(this).text()
+            }, {
+                duration: 1500,
+                easing: 'swing',
+                step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+            });
+            show = false;
+        });
+    }
+});
